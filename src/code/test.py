f758303bd45872b06b489964b5ba3e551ac25c7a
benedekaibas <kaibas.benedek02@gmail.com>
2025-02-03 16:32:12 -0500

    created a test.py file to collect course materials

---
+++ src/code/test.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def softmax(logits):
+    """Implementing the softmax function."""
+    exp_logits = np.exp(logits)
+    softmax_output = exp_logits / np.sum(logits)
+    return softmax_output
+
+def cross_entropy(y_true, y_pred):
+    # implement cross entropy
+    loss_ce = np.sum(y_true * np.log(y_pred))
+    return loss_ce
+
+# example input
+X = np.array([0.2, 0.5, 0.8])
+
+# example weights
+
+W = np.array([
+    [0.5, -0.2, 0.3],
+    [0.1, 0.7, -0.5],
+    [-0.3, 0.2, 0.8],
+])
+
+# create bias 
+b = np.array([0.1, -0.1, 0.2])
+
+# Output example to compare result
+y_true = np.array([1, 0, 0]) # this is gonna be the cat
+
+# calculate raw scores (logits)
+logits = np.dot(X, W, b)
+print(f"Raw scores/output: {logits}")
+
+y_pred = softmax(logits)
+print(f"Softmax: {y_pred}")
+
+loss = cross_entropy(y_true, y_pred)
